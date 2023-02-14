c100e7e19271a2c6705afe1064fa9f6ee0b83fa6
Logan Reynolds <gizmordo@gmail.com>
2023-02-13 21:34:02 -0500

    Finished new page lists

--- tests/new_homepage/index.js
+++ tests/new_homepage/index.js
@@ -1,51 +1,87 @@
+let pageListContainer = document.getElementById("pagelistcontainer");
 let infoText = document.getElementById("infopanel");
 
-// let variable;
-// try {
-
-//   .then(response => response.json())
-//   .then(data => {
-//     variable = data;
-//     alert(variable);
-//   });
-// } catch(e) {
-//   alert(e);
-// }
+let pageListElements = [];
+let pageJson;
+let pages = [];
 
 async function getJson(url) {
-  try {
-  let response = await fetch("pages.json");
+  let response = await fetch(url);
   let data = await response.json();
-  } catch (e) {
-    alert(e);
-  }
+  return data;
 }
 
-getJson();
+function loadPageListContainer() {
+  let pageLists = Object.keys(pageJson);
 
-// let moreInfoPages = Array.from(
-//   document.querySelectorAll(".pagelist:not(#mainlist)")
-// );
-// let listOpeners = Array.from(document.getElementsByClassName("subinfo"));
+  for (let pageListIndex = 0; pageListIndex < pageLists.length; pageListIndex++) {
+    const list = pageLists[pageListIndex];
+    pageListElements[pageListIndex] = loadPageList(list, pageListIndex == 0);
+    pageListContainer.appendChild(pageListElements[pageListIndex]);
+  }
 
-// listOpeners.forEach((element) => {
-//   element.addEventListener("mouseenter", () => {
-//     let page = document.getElementById(element.dataset.referencePage);
-//     let filteredPages = moreInfoPages.filter(
-//       (testPage) => !testPage.classList.contains(page)
-//     );
-//     filteredPages.forEach((element2) => {
-//       element2.style = "display: none";
-//     });
-//     page.style = "display: flex";
-//   });
-// });
+  pages.forEach(page => {
+    let pageData = page.dataset;
+    if (pageData.description != null) {
+      page.addEventListener("mouseenter", () => {
+        infoText.hidden = false;
+        infoText.innerHTML = `${page.innerHTML}: ${pageData.description}`;
+      });
+    }
+    if (pageData.referenceList != null) {
+      page.addEventListener("mouseenter", () => {
+        for (let i = 1; i < pageListElements.length; i++) {
+          const pageList = pageListElements[i];
+          if (pageList.id == pageData.referenceList) {
+            pageList.style = "display: flex";
+          } else {
+            pageList.style = "display: none";
+          }
+        }
+      });
+    }
+  });
+}
 
-// function showInfo(text) {
-//   infoText.innerHTML = text;
-//   infoText.hidden = false;
-// }
+function loadPageList(listKey, isMain) {
+  let listElement = document.createElement("div");
 
-// function hideInfo() {
-//   infoText.hidden = true;
-// }
+  listElement.id = listKey;
+  listElement.classList.add("pagelist");
+
+  if (!isMain) listElement.style = "display: none";
+
+  let pageItemIndexes = Object.keys(pageJson[listKey]);
+  pageItemIndexes.forEach(page => {
+    listElement.appendChild(loadPage(listKey, page));
+  });
+  return listElement;
+}
+
+function loadPage(listKey, pageKey) {
+  let keys = Object.keys(pageJson[listKey][pageKey]);
+
+  let pageElement;
+  if (keys.includes("link")) {
+    pageElement = document.createElement("a");
+    pageElement.href = pageJson[listKey][pageKey]["link"];
+  } else {
+    pageElement = document.createElement("p");
+  }
+
+  pageElement.innerHTML = pageKey;
+  pageElement.classList.add("page");
+
+  keys.forEach(key => {
+    if (key != "link") {
+      pageElement.dataset[key] = pageJson[listKey][pageKey][key];
+    }
+  })
+  pages.push(pageElement);
+  return pageElement;
+}
+
+getJson("pages.json").then(json => {
+  pageJson = json;
+  loadPageListContainer();
+});
