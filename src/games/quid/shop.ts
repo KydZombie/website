0756391098868aeffd23f3a1d454be3d7c2bab84
Logan Reynolds <gizmordo@gmail.com>
2023-02-19 14:53:43 -0500

    Shop icons

--- src/games/quid/shop.ts
+++ src/games/quid/shop.ts
@@ -3,9 +3,25 @@ export class Shop {
     items: HTMLDivElement[] = [];
 
     constructor() {
-        for(let i = 0; i < 10; i++) {
+        for(let i = 0; i < 8; i++) {
             let shopItem = document.createElement("div");
             shopItem.classList.add("shopitem");
+
+            let title = document.createElement("h1");
+            title.innerHTML = "Example Thing";
+
+            let icon = document.createElement("img");
+            icon.src = "assets/item/metalRaw.png";
+
+            let text = document.createElement("p");
+            text.innerHTML = "This is a description";
+
+            shopItem.appendChild(title);
+            shopItem.appendChild(icon);
+            shopItem.appendChild(text);
+            
+            
+
             this.element.appendChild(shopItem);
             this.items.push(shopItem);
         }
